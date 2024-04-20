e8f19350f9a1cb63967c64a8bed98fa35da7d768
Alexander Basov <xxbasovaxx@yandex.ru>
2024-04-20 11:34:02 +0700

    Добавьте файлы проекта.

---
+++ Xamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.ViewModels;
+
+namespace Xamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}

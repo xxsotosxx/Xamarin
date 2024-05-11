82a32f683ea0169c33c7894ceb5381b5fdba3c68
Alexander Basov <xxbasovaxx@yandex.ru>
2024-05-11 10:22:35 +0700

    +Interface

---
+++ Game/Logic/Interfaces.cs
@@ -0,0 +1,23 @@
+﻿using SkiaSharp.Views.Forms;
+using SkiaSharp;
+using System;
+ 
+
+namespace Game.Logic
+{
+    internal interface I2DGraphicMember
+    {
+        void Draw(SKCanvas canvas, SKPaintSurfaceEventArgs args);
+        //void Animate();
+        void SetXY(SKPoint point);
+    }
+
+    internal interface IAnimated
+    {
+        void doAnimate(SKCanvas canvas, SKPaintSurfaceEventArgs args);
+    }
+    internal interface IMapAction
+    {
+        void onCollision(Action<Something, Something> param);
+    }
+}
